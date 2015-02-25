c4f7e9f9062a9500ca4e58549601b7b15fc37728
Randy Tolentino <makertolentino@gmail.com>
2015-02-25 05:50:17 -0600

    main nav up

--- builds/development/js/app.js
+++ builds/development/js/app.js
@@ -1,32 +1,3 @@
-var myApp = angular.module('myApp',
-  ['ngRoute', 'firebase', 'appControllers'])
-.constant('FIREBASE_URL', 'https://scrumcheck.firebaseio.com/');
+var myApp = angular.module('myApp', ['ngRoute', 'appControllers']);
 
-var appControllers = angular.module('appControllers', ['firebase']);
-
-// myApp.config(['$routeProvider', function($routeProvider) {
-//   $routeProvider.
-//     when('/login', {
-//       templateUrl: 'views/login.html',
-//       controller:  'RegistrationController'
-//     }).
-//     when('/register', {
-//       templateUrl: 'views/register.html',
-//       controller:  'RegistrationController'
-//     }).
-//     when('/meetings', {
-//       templateUrl: 'views/meetings.html',
-//       controller:  'MeetingsController'
-//     }).
-//     when('/checkins/:uId/:mId', {
-//       templateUrl: 'views/checkins.html',
-//       controller:  'CheckInsController'
-//     }).
-//     when('/checkins/:uId/:mId/checkinsList', {
-//       templateUrl: 'views/checkinslist.html',
-//       controller:  'CheckInsController'
-//     }).
-//     otherwise({
-//       redirectTo: '/meetings'
-//     });
-// }]);
+var appControllers = angular.module('appControllers',[]);
