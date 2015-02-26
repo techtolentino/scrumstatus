91bbdaef9dbd785a50d882181f4d3150c11e84f2
Randy Tolentino <makertolentino@gmail.com>
2015-02-26 04:19:08 -0600

    all views routed

--- builds/development/js/app.js
+++ builds/development/js/app.js
@@ -6,5 +6,17 @@ myApp.config(['$routeProvider', function($routeProvider){
 	$routeProvider.
 		when('/login', {
 			templateUrl: 'views/login.html'
+		}).
+		when('/team', {
+			templateUrl: 'views/team.html'
+		}).
+		when('/about', {
+			templateUrl: 'views/about.html'
+		}).
+		when('/register', {
+			templateUrl: 'views/register.html'
+		}).
+		otherwise({
+			redirectTo: '/login'
 		});
 }]);
