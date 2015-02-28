bf7496bb2eacce4c26ed9ca5a05ecd23f43dfa88
Randy Tolentino <makertolentino@gmail.com>
2015-02-28 04:36:10 -0600

    moving with dialogs

--- members.js
+++ members.js
@@ -5,80 +5,270 @@
       "firstname": "Michael",
       "lastname": "Jordan",
       "project": "sample project description",
-      "status": "red",
-      "image": "http://telehealth.org/wp-content/images/user-placeholder.jpg"
+      "image": "http://telehealth.org/wp-content/images/user-placeholder.jpg",
+      "upcoming": "PTO on Thursday",
+      "status": {
+        "color":  "red",
+        "contact": {
+          "email": "test@email.com",
+          "yahoo": "yahooIM"
+        },
+        "projects": {
+          "projectone": "project one",
+          "projecttwo": "project two",
+          "projectthree": "project three",
+          "projectfour": "project four",
+          "projectfive": "project five",
+          "projectsix": "project six",
+          "projectseven": "project seven",
+          "projecteight": "project eight",
+          "projectnine": "project nine",
+          "projectten": "project ten"
+        }
+      }
     },
     "-JWT65QvjwD4TSFjDx4V": {
       "date": 1410328192928,
       "firstname": "Magic",
       "lastname": "Johnson",
       "project": "sample project description",
-      "status": "yellow",
-      "image": "http://telehealth.org/wp-content/images/user-placeholder.jpg"
+      "image": "http://telehealth.org/wp-content/images/user-placeholder.jpg",
+      "upcoming": "PTO on Thursday",
+      "status": {
+        "color":  "yellow",
+        "contact": {
+          "email": "test@email.com",
+          "yahoo": "yahooIM"
+        },
+        "projects": {
+          "projectone": "project one",
+          "projecttwo": "project two",
+          "projectthree": "project three",
+          "projectfour": "project four",
+          "projectfive": "project five",
+          "projectsix": "project six",
+          "projectseven": "project seven",
+          "projecteight": "project eight",
+          "projectnine": "project nine",
+          "projectten": "project ten"
+        }
+      }
     },
     "-JWk3R8ejXlaOVBr7hxd": {
       "date": 1410629485228,
       "firstname": "Larry",
       "lastname": "Bird",
       "project": "sample project description",
-      "status": "green",
-      "image": "http://telehealth.org/wp-content/images/user-placeholder.jpg"
+      "image": "http://telehealth.org/wp-content/images/user-placeholder.jpg",
+      "upcoming": "PTO on Thursday",
+      "status": {
+        "color":  "green",
+        "contact": {
+          "email": "test@email.com",
+          "yahoo": "yahooIM"
+        },
+        "projects": {
+          "projectone": "project one",
+          "projecttwo": "project two",
+          "projectthree": "project three",
+          "projectfour": "project four",
+          "projectfive": "project five",
+          "projectsix": "project six",
+          "projectseven": "project seven",
+          "projecteight": "project eight",
+          "projectnine": "project nine",
+          "projectten": "project ten"
+        }
+      }
     },
     "-JX0jlHRCfiI6DiDnySn": {
       "date": 1410926056676,
       "firstname": "Charles",
       "lastname": "Barkley",
       "project": "sample project description",
-      "status": "orange",
-      "image": "http://telehealth.org/wp-content/images/user-placeholder.jpg"
+      "image": "http://telehealth.org/wp-content/images/user-placeholder.jpg",
+      "upcoming": "PTO on Thursday",
+      "status": {
+        "color":  "orange",
+        "contact": {
+          "email": "test@email.com",
+          "yahoo": "yahooIM"
+        },
+        "projects": {
+          "projectone": "project one",
+          "projecttwo": "project two",
+          "projectthree": "project three",
+          "projectfour": "project four",
+          "projectfive": "project five",
+          "projectsix": "project six",
+          "projectseven": "project seven",
+          "projecteight": "project eight",
+          "projectnine": "project nine",
+          "projectten": "project ten"
+        }
+      }
     },
     "-JX0jlHRCfidafee23yS": {
       "date": 1410926056676,
       "firstname": "Shawn",
       "lastname": "Kemp",
       "project": "sample project description",
-      "status": "grellow",
-      "image": "http://telehealth.org/wp-content/images/user-placeholder.jpg"
+      "image": "http://telehealth.org/wp-content/images/user-placeholder.jpg",
+      "upcoming": "PTO on Thursday",
+      "status": {
+        "color":  "grellow",
+        "contact": {
+          "email": "test@email.com",
+          "yahoo": "yahooIM"
+        },
+        "projects": {
+          "projectone": "project one",
+          "projecttwo": "project two",
+          "projectthree": "project three",
+          "projectfour": "project four",
+          "projectfive": "project five",
+          "projectsix": "project six",
+          "projectseven": "project seven",
+          "projecteight": "project eight",
+          "projectnine": "project nine",
+          "projectten": "project ten"
+        }
+      }
     },
     "-JWT65QvjwadfeaFjDx4": {
       "date": 1410328192928,
       "firstname": "James",
       "lastname": "Worthy",
       "project": "sample project description",
-      "status": "yellow",
-      "image": "http://telehealth.org/wp-content/images/user-placeholder.jpg"
+      "image": "http://telehealth.org/wp-content/images/user-placeholder.jpg",
+      "upcoming": "PTO on Thursday",
+      "status": {
+        "color":  "yellow",
+        "contact": {
+          "email": "test@email.com",
+          "yahoo": "yahooIM"
+        },
+        "projects": {
+          "projectone": "project one",
+          "projecttwo": "project two",
+          "projectthree": "project three",
+          "projectfour": "project four",
+          "projectfive": "project five",
+          "projectsix": "project six",
+          "projectseven": "project seven",
+          "projecteight": "project eight",
+          "projectnine": "project nine",
+          "projectten": "project ten"
+        }
+      }
     },
     "-JWT65QvjwD4TSjwadfd": {
       "date": 1410328192928,
       "firstname": "Clyde",
       "lastname": "Drexler",
       "project": "sample project description",
-      "status": "darkred",
-      "image": "http://telehealth.org/wp-content/images/user-placeholder.jpg"
+      "image": "http://telehealth.org/wp-content/images/user-placeholder.jpg",
+      "upcoming": "PTO on Thursday",
+      "status": {
+        "color":  "darkred",
+        "contact": {
+          "email": "test@email.com",
+          "yahoo": "yahooIM"
+        },
+        "projects": {
+          "projectone": "project one",
+          "projecttwo": "project two",
+          "projectthree": "project three",
+          "projectfour": "project four",
+          "projectfive": "project five",
+          "projectsix": "project six",
+          "projectseven": "project seven",
+          "projecteight": "project eight",
+          "projectnine": "project nine",
+          "projectten": "project ten"
+        }
+      }
     },
     "-JWjwadfeaD4TSFjDx4V": {
       "date": 1410328192928,
       "firstname": "David",
       "lastname": "Robinson",
       "project": "sample project description",
-      "status": "grellow",
-      "image": "http://telehealth.org/wp-content/images/user-placeholder.jpg"
+      "image": "http://telehealth.org/wp-content/images/user-placeholder.jpg",
+      "upcoming": "PTO on Thursday",
+      "status": {
+        "color":  "grellow",
+        "contact": {
+          "email": "test@email.com",
+          "yahoo": "yahooIM"
+        },
+        "projects": {
+          "projectone": "project one",
+          "projecttwo": "project two",
+          "projectthree": "project three",
+          "projectfour": "project four",
+          "projectfive": "project five",
+          "projectsix": "project six",
+          "projectseven": "project seven",
+          "projecteight": "project eight",
+          "projectnine": "project nine",
+          "projectten": "project ten"
+        }
+      }
     },
     "-JjwadfeawD4TSFjDx4V": {
       "date": 1410328192928,
       "firstname": "Alonzo",
       "lastname": "Morning",
       "project": "sample project description",
-      "status": "orange",
-      "image": "http://telehealth.org/wp-content/images/user-placeholder.jpg"
+      "image": "http://telehealth.org/wp-content/images/user-placeholder.jpg",
+      "upcoming": "PTO on Thursday",
+      "status": {
+        "color":  "orange",
+        "contact": {
+          "email": "test@email.com",
+          "yahoo": "yahooIM"
+        },
+        "projects": {
+          "projectone": "project one",
+          "projecttwo": "project two",
+          "projectthree": "project three",
+          "projectfour": "project four",
+          "projectfive": "project five",
+          "projectsix": "project six",
+          "projectseven": "project seven",
+          "projecteight": "project eight",
+          "projectnine": "project nine",
+          "projectten": "project ten"
+        }
+      }
     },
     "-JWT65QvjwDjwadfea4V": {
       "date": 1410328192928,
       "firstname": "Isiah",
       "lastname": "Thomas",
       "project": "sample project description",
-      "status": "green",
-      "image": "http://telehealth.org/wp-content/images/user-placeholder.jpg"
+      "image": "http://telehealth.org/wp-content/images/user-placeholder.jpg",
+      "upcoming": "PTO on Thursday",
+      "status": {
+        "color":  "green",
+        "contact": {
+          "email": "test@email.com",
+          "yahoo": "yahooIM"
+        },
+        "projects": {
+          "projectone": "project one",
+          "projecttwo": "project two",
+          "projectthree": "project three",
+          "projectfour": "project four",
+          "projectfive": "project five",
+          "projectsix": "project six",
+          "projectseven": "project seven",
+          "projecteight": "project eight",
+          "projectnine": "project nine",
+          "projectten": "project ten"
+        }
+      }
     }
   }
 }
