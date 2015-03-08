75e138c77fa7f2e0266e81fbd2c792f05633b9f2
Randy Tolentino <makertolentino@gmail.com>
2015-03-08 18:09:34 -0500

    good place for adding users in the registration controller

--- builds/development/js/controllers/registration.js
+++ builds/development/js/controllers/registration.js
@@ -1,27 +1,23 @@
-// User Registration Controller
 myApp.controller('RegistrationController', 
-	function($scope, $firebaseSimpleLogin, $location, FIREBASE_URL, Authentication){
+  function($scope, $firebaseSimpleLogin, $location, Authentication) {
+  
+  $scope.login = function() {
+    Authentication.login($scope.user)
+      .then(function(user) {
+      $location.path('/meetings');
+    }, function(error) {
+      $scope.message = error.toString();
+    });
+  } //login
 
-		var ref = new Firebase(FIREBASE_URL);
+  $scope.register = function() {
+    Authentication.register($scope.user)
+      .then(function(user) {
+      Authentication.login($scope.user);
+      $location.path('/meetings');
+    }, function(error) {
+      $scope.message = error.toString();
+    });
+  } //login
 
-		var simpleLogin = $firebaseSimpleLogin(ref);
-
-		$scope.login = function() {
-			Authentication.login($scope.user)
-			.then(function(user) {
-				$location.path('/team');
-			}, function(error){
-				$scope.message = error.toString();
-			});
-		} //login
-
-		$scope.register = function() {
-			Authentication.register($scope.user)
-			.then(function(user) {
-				Authentication.login($scope.user);
-				$location.path('/team');
-			}, function(error){
-				$scope.message = error.toString();
-			});
-		} //login
-});
+}); //RegistrationController
--- builds/development/js/services/authentication.js
+++ builds/development/js/services/authentication.js
@@ -1,42 +1,59 @@
-myApp.factory('Authentication', 
-	function($firebase, $firebaseSimpleLogin, $location, FIREBASE_URL){
-	
-		var ref = new Firebase(FIREBASE_URL);
-		var simpleLogin = $firebaseSimpleLogin(ref);
+myApp.factory('Authentication', function($firebase, 
+  $firebaseSimpleLogin, FIREBASE_URL, $rootScope, $location) {
 
-		var myObject = {
-			login: function(user) {
-				return simpleLogin.$login('password', {
-					email: user.email,
-					password: user.password
-				});
-			}, //login
+  var ref = new Firebase(FIREBASE_URL);
+  var simpleLogin = $firebaseSimpleLogin(ref);
 
+  var myObject = {
 
-			register: function(user) {
-				return simpleLogin.$login('password', {
-					email: user.email,
-					password: user.password
-				});
-			}, //register
+    login : function(user) {
 
-			logout: function() {
-				return simpleLogin.$logout();
-			}, //logout
+      var userRef = new Firebase(FIREBASE_URL + '/users/' + user.uid);
+      var userObj = $firebase(userRef).$asObject();
 
-			signedIn: function() {
-				return simpleLogin.user != null;
-			}
+      userObj.$loaded().then(function() {
+        $rootScope.currentUser = userObj;
+      });
 
-			// add the function to the rootScope
+      return simpleLogin.$login('password', {
+        email: user.email,
+        password: user.password
+      });
+    }, //login
 
-			$rootScope.signedIn = function() {
-				return myObject.signedIn();
-			}
+    register : function(user) {
+      return simpleLogin.$createUser(user.email, user.password)
+      .then(function(regUser){
+        var ref = new Firebase(FIREBASE_URL + 'users');
+        var firebaseUsers = $firebase(ref);
 
+        var userInfo = {
+          date: Firebase.ServerValue.TIMESTAMP,
+          regUser: regUser.uid,
+          firstname: user.firstname,
+          lastname: user.lastname,
+          email: user.email
+        }
 
+        firebaseUsers.$set(regUser.uid, userInfo);
+      }); //add user
+    }, //register
 
-		} //myObject
+    logout : function() {
+      return simpleLogin.$logout();
+    }, //logout
 
-		return myObject;
+    signedIn: function() {
+      return simpleLogin.user != null;
+    }
+
+  } //myObject
+
+  //add the function to the rootScope
+
+  $rootScope.signedIn = function() {
+    return myObject.signedIn();
+  }
+
+  return myObject;
 });
