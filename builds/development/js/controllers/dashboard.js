925c9c04a942a8fda2314b82701b5baa3c559b08
Randy Tolentino <makertolentino@gmail.com>
2015-03-08 18:45:17 -0500

    registration function working wonderfully, now adds a full object to Firebase and all routes work with that Object

--- builds/development/js/controllers/dashboard.js
+++ builds/development/js/controllers/dashboard.js
@@ -1,9 +1,8 @@
 myApp.controller('DashboardController', 
 	function($scope, $firebase, $routeParams, $location, FIREBASE_URL){
 
-	var whichTeam = $routeParams.uId;
-	var whichMember = $routeParams.mId;
-	var ref = new Firebase(FIREBASE_URL + 'users/' + '/' + whichTeam + '/' + whichMember);
+	var whichMember = $routeParams.uId;
+	var ref = new Firebase(FIREBASE_URL + 'users/' + '/' + whichMember);
 	var member = $firebase(ref);
 
 	$scope.member = member.$asObject();
